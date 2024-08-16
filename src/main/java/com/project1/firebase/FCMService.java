9fd8ec4faef508fc80b1aac1f1d04f1f6c431d3a
abdallahkrj <abdallahkrj@gmail.com>
2024-08-17 01:09:46 +0300

    fcm

--- src/main/java/com/project1/firebase/FCMService.java
+++ src/main/java/com/project1/firebase/FCMService.java
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-//package com.project1.firebase;
-//
-//import com.google.firebase.messaging.FirebaseMessaging;
-//import com.google.firebase.messaging.Message;
-//import com.google.firebase.messaging.Notification;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class FCMService {
-//
-//    public void sendNotification(String title, String body, String token) throws Exception {
-//        Message message = Message.builder()
-//                .setToken(token)
-//                .setNotification(Notification.builder()
-//                        .setTitle(title)
-//                        .setBody(body)
-//                        .build())
-//                .build();
-//
-//        String response = FirebaseMessaging.getInstance().send(message);
-//        System.out.println("Successfully sent message: " + response);
-//    }
-//}
-=======
 package com.project1.firebase;
 
 import com.google.firebase.messaging.FirebaseMessaging;
@@ -49,4 +24,3 @@ public class FCMService {
         System.out.println("Successfully sent message: " + response);
     }
 }
->>>>>>> ca423deedfae5d7b33e5c8ac49d9beba6f438d26
